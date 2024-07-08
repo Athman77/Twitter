c8b222e110ff5ae7a7183de8444c6736b9fb741c
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-08 21:22:48 +0300

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,20 +1,14 @@
 const config = {
-apiKey: "AIzaSyBbKnPwO-u-phkPZSvqTJGClH1INXbfR-g",
-  authDomain: "twitter-c3dda.firebaseapp.com",
-  projectId: "twitter-c3dda",
-  storageBucket: "twitter-c3dda.appspot.com",
-  messagingSenderId: "143101923152",
-  appId: "1:143101923152:web:4686d43c5dbc2172702655",
-  measurementId: "G-P8F2HKJMVX"
+
 
   
- // apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//  appId: process.env.NEXT_PUBLIC_APP_ID,
-//  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+ apiKey: process.env.NEXT_PUBLIC_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+ storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_APP_ID,
+ measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 } as const;
 
 type Config = typeof config;
